0e67e1495aab913aa5c282c073e645d99ed4f00f
joaovitor <jvmedeiros2010@hotmail.com>
2024-02-02 17:13:27 -0300

    feat(views): create login and items views

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     resources :todo_items
   end
   devise_for :users
-
+  get '/home/joaovitor/todo_ruby/app/views/todo_items/index_concluidos.html.erb', to: 'todo_items#index'
+  
   root "todo_lists#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
